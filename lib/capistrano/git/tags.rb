7f25c2cac5c567bcc0bed6ef3cdc47e13255439a
Leon Berenschot <leonb@beriedata.nl>
2010-05-06 15:15:41 +0200

    Version 0.0.2

--- lib/capistrano/git/tags.rb
+++ lib/capistrano/git/tags.rb
@@ -6,41 +6,41 @@ require 'capistrano'
 
 Capistrano::Configuration.instance.load do
 
-    after  "deploy:restart", "git:tags:push_deploy_tag"
-    before "deploy:cleanup", "git:tags:cleanup_deploy_tag"
+  after  "deploy:restart", "git:tags:push_deploy_tag"
+  before "deploy:cleanup", "git:tags:cleanup_deploy_tag"
 
-    namespace :git do
+  namespace :git do
 
-      namespace :tags do
+    namespace :tags do
 
-        desc "Place release tag into Git and push it to server."
-        task :push_deploy_tag do
-          user = `git config --get user.name`
-          email = `git config --get user.email`
+      desc "Place release tag into Git and push it to server."
+      task :push_deploy_tag do
+        user = `git config --get user.name`
+        email = `git config --get user.email`
 
-          puts `git tag #{rails_env}_#{release_name} #{revision} -m "Deployed by #{user} <#{email}>"`
-          puts `git push --tags`
-        end
+        puts `git tag #{rails_env}_#{release_name} #{revision} -m "Deployed by #{user} <#{email}>"`
+        puts `git push --tags`
+      end
 
-        desc "Place release tag into Git and push it to server."
-        task :cleanup_deploy_tag do
-          count = fetch(:keep_releases, 5).to_i
-          if count >= releases.length
-            logger.important "no old release tags to clean up"
-          else
-            logger.info "keeping #{count} of #{releases.length} release tags"
+      desc "Place release tag into Git and push it to server."
+      task :cleanup_deploy_tag do
+        count = fetch(:keep_releases, 5).to_i
+        if count >= releases.length
+          logger.important "no old release tags to clean up"
+        else
+          logger.info "keeping #{count} of #{releases.length} release tags"
 
-            tags = (releases - releases.last(count)).map { |release| "#{rails_env}_#{release}" }
+          tags = (releases - releases.last(count)).map { |release| "#{rails_env}_#{release}" }
 
-            tags.each do |tag|
-              `git tag -d #{tag}`
-              `git push origin :refs/tags/#{tag}`
-            end
+          tags.each do |tag|
+            `git tag -d #{tag}`
+            `git push origin :refs/tags/#{tag}`
           end
         end
-
       end
 
     end
+
   end
+
 end
